294c5379e3f2459257412d152f0a4e7638e3a683
Tony Truong <tony.truong@bcgdv.com>
2018-08-31 14:30:33 +1000

    added web security group

--- postgres.tf
+++ postgres.tf
@@ -5,7 +5,6 @@ resource "aws_rds_cluster" "default" {
   master_username           = "${var.postgres_user}"
   master_password           = "${var.postgres_password}"
   backup_retention_period   = 5
-  #vpc_security_group_ids    = ["${aws_security_group.aurora-sg.id}"]
   db_subnet_group_name      = "${aws_db_subnet_group.aurora.name}"
 }
 resource "aws_rds_cluster_instance" "cluster_instances" {
@@ -24,25 +23,6 @@ resource "aws_db_subnet_group" "aurora" {
       Namespace = "${var.namespace}"
   }
 }
-
-# resource "aws_security_group" "aurora-sg" {
-#   name   = "aurora-security-group"
-#   vpc_id = "${aws_vpc.default.id}"
-
-#   ingress {
-#     protocol    = "tcp"
-#     from_port   = 5432
-#     to_port     = 5432
-#     cidr_blocks = []
-#   }
-
-#   egress {
-#     protocol    = -1
-#     from_port   = 0 
-#     to_port     = 0 
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
 
 output "cluster_address" {
     value = "${aws_rds_cluster.default.endpoint}"
--- vpc.tf
+++ vpc.tf
@@ -14,7 +14,47 @@ resource "aws_subnet" "private" {
   cidr_block        = "10.0.${count.index}.0/24"
 }
 
+resource "aws_security_group" "web" {
+    name = "vpc_web"
+    description = "Allow incoming HTTP connections."
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+    vpc_id = "${aws_vpc.default.id}"
+
+    tags {
+        Name = "WebServerSG"
+    }
+}
+
 output "VPC ARN" {
     value = "${aws_vpc.default.arn}"
+}
+
+output "Web SG ARN" {
+    value = "${aws_security_group.web.arn}"
 }
 
